246c022e8c87576e6db47e5d2a3339940419a23c
taren <taren.lewis@gmail.com>
2020-06-11 12:57:37 -0500

    Initial Commit

---
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Controllers
+{
+    // values controller example request
+    // http:localhost:5000/api/values 
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // Because this is a private field, it is standard practice to
+        // include a '_' character in front of the field name.
+        private readonly DataContext _context;
+
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+
+        }
+
+        // GET api/values
+        // IEnumerable is a collection of "things", in this case a collection
+        // of strings
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+            // Go to DB, get Values as a list, and store them in 'values'
+            // with an HTTP 200 OK response. 
+            var values = await _context.Values.ToListAsync();
+
+            return Ok(values);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+            // FirstOrDefault returns default null value if an id is not found,
+            // in order to avoid an expensive exception.
+            var value = await _context.Values.FirstOrDefaultAsync(x => x.Id == id);
+            return Ok(value);
+        }
+
+        // POST api/values
+        // (used for creating a record)
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        // (used for editing a record)
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        // (used for deleting a record)
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options){}
+    
+    // Name "Values" used to represent the table name that gets created when the database is "Scaffolded"
+        public DbSet<Value> Values { get; set; }
+    
+    }
+}
